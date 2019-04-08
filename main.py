761f3f760459920891715a02767850537255a3ca
Christian van Dijk <christian.vandijk@student.hu.nl>
2019-04-08 21:17:43 +0200

    tqdm added, locations saved to JSON now, geo_service extended with json and object functions

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from tqdm import tqdm
 
 geo_service = GeoService()
 geo_service.add_location("thuis", "Tweede Westerparklaan", "202")
-geo_service.add_location("vriendin", "Monarchvlinderlaan", "19")
+geo_service.add_location("vriendin", "Mondriaanerf", "19")
+geo_service.add_location("werk", "Atoomweg", "63")
 
 geo_service.save_all_locations()
 
-for l in tqdm(geo_service.get_all_locations()):
-    print(l.display_name)
+print('Locations')
+for l in geo_service.get_all_locations():
+    print("- "+l.name+": "+l.display_name)
